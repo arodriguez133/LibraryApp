38194d67a1d4061dfee9a78ae7ac0b3f112435c8
arodriguez133 <arodriguez133@icloud.com>
2022-08-29 21:53:17 -0500

    playing with the idea of object contructors and the prototype. I'm working to understand how these things work

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
 let myLibrary = [];
 
-function Book(title, author, pages, read) {
+function Book() {
+}
+
+Book.prototype.info = function () {
+    return `${this.title} by ${this.author} has ${this.pages} pages, ${this.read}`
+}
+
+function Book1(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
 }
 
+Book1.prototype = Object.create(Book.prototype);
+
+let book1 = new Book1('Lord of The Rings', 'JRR Tolkein', '1178', 'has not read');
 
 
-function addBookToLibrary() {
 
+function addBookToLibrary(book) {
+    if (myLibrary.length > 1) {
+        return ` ${myLibrary.push(book)} book's in the library`
+    } else {
+        return ` ${myLibrary.push(book)} book in the library`
+    };
 }
+
+console.log(addBookToLibrary(book1));
+console.log(myLibrary);

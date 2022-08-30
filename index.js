8e5bcd8a7777390e04f0f9daa7a02c348a9f8435
arodriguez133 <arodriguez133@icloud.com>
2022-08-29 22:05:46 -0500

    wip

--- index.js
+++ index.js
@@ -1,32 +1,29 @@
 let myLibrary = [];
 
-function Book() {
-}
-
-Book.prototype.info = function () {
-    return `${this.title} by ${this.author} has ${this.pages} pages, ${this.read}`
-}
-
-function Book1(title, author, pages, read) {
+function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
 }
 
-Book1.prototype = Object.create(Book.prototype);
+Book.prototype.info = function () {
+    return `${this.title} by ${this.author} has ${this.pages} pages, ${this.read}`
+}
 
-let book1 = new Book1('Lord of The Rings', 'JRR Tolkein', '1178', 'has not read');
+
+
+
+
 
 
 
 function addBookToLibrary(book) {
-    if (myLibrary.length > 1) {
-        return ` ${myLibrary.push(book)} book's in the library`
-    } else {
-        return ` ${myLibrary.push(book)} book in the library`
-    };
+    myLibrary.push(book);
+    return myLibrary;
 }
 
-console.log(addBookToLibrary(book1));
-console.log(myLibrary);
+
+console.log(addBookToLibrary(new Book('Lord of The Rings', 'JRR Tolkein', '1178', 'has not read')));
+
+
